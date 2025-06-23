4884a5924a92d15506e3626de0861abfebe7c97c
mark <mmckenzie-nat@gmail.com>
2025-06-23 15:53:05 +1000

    first commit

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,6 +3,25 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
+  const results = [];
+  const pending = [];
+
+  urls.forEach(url => {
+    const data = httpGet(url);
+    pending.push(data);
+  });
+
+  const arr = await Promise.all(pending);
+
+  arr.forEach(({ status, body}) => {
+    const { message } = JSON.parse(body);
+    if (status === 200) {    
+      results.push({'Arnie Quote': message})
+    } else {
+      results.push({'FAILURE': message })
+    }
+  });
+  return results;
 };
 
 module.exports = {
