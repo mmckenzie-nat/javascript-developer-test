8fc669d4800adfabae8e0f24ff9dd167e1a62be3
mmckenzie-nat <mmckenzie-nat@gmail.com>
2025-06-23 19:13:26 +1000

    simplify

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,27 +1,19 @@
 const { httpGet } = require('./mock-http-interface');
 
+const wrapper = async (url) => 
+  httpGet(url).then(({ status, body }) => {
+    const key = (status === 200) ? 'Arnie Quote' : 'FAILURE';
+    const message = JSON.parse(body);
+    return { [key]: message };
+  });
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-  const results = [];
   const pending = [];
-
   urls.forEach(url => {
-    const data = httpGet(url);
-    pending.push(data);
+    const data = wrapper(url);
+    pending.push(data)
   });
-
-  const arr = await Promise.all(pending);
-
-  arr.forEach(({ status, body}) => {
-    const { message } = JSON.parse(body);
-    if (status === 200) {    
-      results.push({'Arnie Quote': message})
-    } else {
-      results.push({'FAILURE': message })
-    }
-  });
-  return results;
+  return Promise.all(pending);
 };
 
 module.exports = {
